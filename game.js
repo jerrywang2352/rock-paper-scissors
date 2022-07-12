99636764e2623cbd5e39ec8bcf1e66fb682b9c27
Jerry Wang <jerry.wang2352@gmail.com>
2022-07-12 04:13:44 -0400

    Write main game function

--- game.js
+++ game.js
@@ -1,3 +1,5 @@
+game(); 
+
 function computerPlay() {
     random = Math.floor(Math.random()*3);
     if (random == 0) {
@@ -37,3 +39,30 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
+function game() {
+    let playerCount = 0;
+    let computerCount = 0; 
+    for (let i = 0; i < 5; i++) {
+        let playerSelection = prompt("Rock, Paper, or Scissors?");
+        let computerSelection = computerPlay(); 
+        let results = playRound(playerSelection,computerSelection); 
+        console.log(`Player chose: ${playerSelection}`); 
+        console.log(`Computer chose: ${computerSelection}`); 
+        console.log(results);
+        console.log("-------------------------------");
+        if (results.slice(0,9) == "You Lose!") {
+            computerCount ++; 
+        } else if (results.slice(0,8) == "You Win!") {
+            playerCount ++; 
+        } 
+    } 
+
+    //evaluate results of the 5 rounds 
+    if (playerCount > computerCount) {
+        console.log("Player has won!!!!"); 
+    } else if (playerCount < computerCount) {
+        console.log("Computer has won!!!!");
+    } else {
+        console.log("Draw!!!");
+    }
+}
