44c3e6d38eff8c7537cd71a30ac8c25b6fe4e106
Jerry Wang <jerry.wang2352@gmail.com>
2022-07-12 03:59:29 -0400

    Add playRound to game.js

--- game.js
+++ game.js
@@ -9,3 +9,31 @@ function computerPlay() {
     }
 }
 
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection == "Rock") {
+        if (computerSelection == "Paper") {
+            return "You Lose! Paper beats Rock!"; 
+        } else if (computerSelection =="Scissors") {
+            return "You Win! Rock beats Scissors!"; 
+        } else {
+            return "Draw!!"; 
+        }
+    } else if (playerSelection == "Paper") {
+        if (computerSelection == "Scissors") {
+            return "You Lose! Scissors beats Paper!"; 
+        } else if (computerSelection =="Rock") {
+            return "You Win! Paper beats Rock!"; 
+        } else {
+            return "Draw!!"; 
+        }        
+    } else {
+        if (computerSelection == "Rock") {
+            return "You Lose! Rock beats Scissors!"; 
+        } else if (computerSelection =="Paper") {
+            return "You Win! Scissors beats Paper!"; 
+        } else {
+            return "Draw!!"; 
+        }
+    }
+}
+
